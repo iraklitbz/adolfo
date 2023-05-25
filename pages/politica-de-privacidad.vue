a2b88e459243f58aa35f45e785fadfa4928b88e4
irakli Tavberidze <irakli.tavberidze@tuispain.com>
2023-05-25 10:01:34 +0200

    added policy confiramtion

---
+++ pages/politica-de-privacidad.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="mt-32 overflow-hidden">
+      <Header/>
+            <div
+                class="max-w-4xl m-auto justify-between relative z-40 px-5"
+            >
+                <h2 class="main-headline text-xl px-4 md:px-0 md:text-3xl text-primary my-10 block font-bold">
+                    Política de Privacidad
+                </h2>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Agradecemos tu interés en nuestro sitio web y en suscribirte a nuestra newsletter. La protección de tu privacidad es de suma importancia para nosotros. A continuación, te explicamos cómo recopilamos, utilizamos y protegemos la información personal que nos proporcionas.
+                </p>
+                <h3 class="main-headline text-lg px-4 md:px-0 md:text-2xl text-primary my-10 block font-bold">
+                    Recopilación de información
+                </h3>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Cuando te suscribes a nuestra newsletter, podemos recopilar la siguiente información personal:
+                </p>
+                <ul class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    <li>
+                        Nombre y apellidos
+                    </li>
+                    <li>
+                        Dirección de correo electrónico
+                    </li>
+                </ul>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Además, podemos recopilar información no personal, como datos demográficos, intereses o preferencias, de forma anónima.
+                </p>
+                <h3 class="main-headline text-lg px-4 md:px-0 md:text-2xl text-primary my-10 block font-bold">
+                    Uso de la información
+                </h3>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    La información personal que recopilamos se utiliza exclusivamente para los siguientes fines:
+                </p>
+                <ul class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    <li>
+                        Enviarte nuestra newsletter con contenido relevante, promociones y noticias relacionadas con nuestros productos y servicios.
+                    </li>
+                    <li>
+                        Personalizar el contenido de la newsletter en función de tus intereses y preferencias.
+                    </li>
+                    <li>
+                        Mejorar nuestros productos y servicios, así como la experiencia del usuario en nuestro sitio web.
+                    </li>
+                </ul>
+                <h2 class="main-headline text-xl px-4 md:px-0 md:text-3xl text-primary my-10 block font-bold">
+                    Base legal para el tratamiento de datos
+                </h2>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    El tratamiento de tus datos personales se basa en tu consentimiento, el cual nos proporcionas al suscribirte a nuestra newsletter. Tienes el derecho de retirar tu consentimiento en cualquier momento, lo que implicará que dejaremos de utilizar tus datos personales para los fines mencionados anteriormente.
+                </p>
+                <h2 class="main-headline text-xl px-4 md:px-0 md:text-3xl text-primary my-10 block font-bold">
+                    Compartir información con terceros
+                </h2>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    No compartimos tu información personal con terceros sin tu consentimiento, excepto en los siguientes casos:
+                </p>
+                <ul class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    <li>
+                        Cuando sea necesario para cumplir con la legislación aplicable o responder a una orden judicial.
+                    </li>
+                    <li>
+                        Cuando sea necesario para proteger nuestros derechos, propiedad o seguridad, así como los derechos, propiedad o seguridad de nuestros usuarios u otras partes interesadas.
+                    </li>
+                </ul>
+                <h2 class="main-headline text-xl px-4 md:px-0 md:text-3xl text-primary my-10 block font-bold">
+                    Seguridad de la información
+                </h2>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Implementamos medidas de seguridad adecuadas para proteger tu información personal contra accesos no autorizados, modificaciones, divulgaciones o destrucciones. Sin embargo, debes tener en cuenta que ninguna transmisión de datos a través de internet o sistema de almacenamiento electrónico puede garantizar una seguridad absoluta.
+                </p>
+                <h2 class="main-headline text-xl px-4 md:px-0 md:text-3xl text-primary my-10 block font-bold">
+                    Tus derechos
+                </h2>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Como titular de los datos personales, tienes ciertos derechos que puedes ejercer en relación con el tratamiento de tus datos, incluyendo el acceso, rectificación, supresión, limitación, oposición y portabilidad de los mismos. Puedes ejercer tus derechos enviándonos una solicitud por escrito a través de los medios de contacto indicados al final de esta política.
+                </p>
+                <h2 class="main-headline text-xl px-4 md:px-0 md:text-3xl text-primary my-10 block font-bold">
+                    Modificaciones a la política de privacidad
+                </h2>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Nos reservamos el derecho de modificar esta política de privacidad en cualquier momento. Te recomendamos revisar periódicamente esta página para estar al tanto de las actualizaciones. Los cambios entrarán en vigencia a partir de su publicación en el sitio web.
+                </p>
+                <h2 class="main-headline text-xl px-4 md:px-0 md:text-3xl text-primary my-10 block font-bold">
+                    Contacto
+                </h2>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Si tienes alguna pregunta, comentario o solicitud relacionada con esta política de privacidad, no dudes en contactarnos a través de los siguientes medios:
+                </p>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    <b>www.adolfobianco.com</b>
+                </p>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Correo electrónico: <a target="_blank" href="mailto:info@adolfobianco.com" class="">info@adolfobianco.com</a>
+                </p>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Nuestro equipo estará encantado de brindarte la información adicional que necesites y atender tus inquietudes.
+                </p>
+                <p class="text-lg px-4 md:px-0 md:text-xl text-primary my-10 block font-thin">
+                    Gracias por confiar en nosotros y por suscribirte a nuestra newsletter.
+                </p>
+            </div>
+        <Footer />
+    </div>
+  </template>
+  
+  
